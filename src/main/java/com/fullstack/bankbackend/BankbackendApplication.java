618f083f7493802bbc6485aa4e35dbb27a5cd42f
DanielSve <e.danielsvensson@gmail.com>
2022-08-22 11:00:58 +0200

    first commit

---
+++ src/main/java/com/fullstack/bankbackend/BankbackendApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.bankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankbackendApplication.class, args);
+    }
+
+}
