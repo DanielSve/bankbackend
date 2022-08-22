51e6efb8deacd50291082ba7a3b180e800180f40
Roberto Mendez <roberto.mendez1983@gmail.com>
2022-08-22 14:32:15 +0200

    Implementation av Client Service

--- src/main/java/com/fullstack/bankbackend/service/ClientServiceImp.java
+++ src/main/java/com/fullstack/bankbackend/service/ClientServiceImp.java
@@ -1,5 +1,20 @@
 package com.fullstack.bankbackend.service;
 
-public class ClientServiceImp {
+import com.fullstack.bankbackend.model.Client;
+import com.fullstack.bankbackend.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class ClientServiceImp implements ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Override
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
 }
