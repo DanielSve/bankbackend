08f35bd0b4ba16f1204e50adbf933de6fbbe1075
Roberto Mendez <roberto.mendez1983@gmail.com>
2022-08-22 14:31:02 +0200

    Client Service created

--- src/main/java/com/fullstack/bankbackend/service/ClientService.java
+++ src/main/java/com/fullstack/bankbackend/service/ClientService.java
@@ -1,4 +1,12 @@
 package com.fullstack.bankbackend.service;
 
+
+import com.fullstack.bankbackend.model.Client;
+
+import java.util.List;
+
 public interface ClientService {
+
+    public List<Client> getAllClients();
+
 }
