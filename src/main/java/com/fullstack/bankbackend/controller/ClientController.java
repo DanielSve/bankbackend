1d647e77100eec2118c84c6b135c9b8d7c3b0f79
Roberto Mendez <roberto.mendez1983@gmail.com>
2022-08-22 14:29:28 +0200

    Added getAll method

--- src/main/java/com/fullstack/bankbackend/controller/ClientController.java
+++ src/main/java/com/fullstack/bankbackend/controller/ClientController.java
@@ -1,4 +1,23 @@
 package com.fullstack.bankbackend.controller;
 
+import com.fullstack.bankbackend.model.Client;
+import com.fullstack.bankbackend.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clients")
 public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping("")
+    private List<Client> getAllClients() {
+        return clientService.getAllClients();
+    }
 }
