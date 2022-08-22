d8ffc7f1950019a6ef15119fe388545253f9835d
Roberto Mendez <roberto.mendez1983@gmail.com>
2022-08-22 14:32:59 +0200

    Client Repository created

--- src/main/java/com/fullstack/bankbackend/repository/ClientRepository.java
+++ src/main/java/com/fullstack/bankbackend/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package com.fullstack.bankbackend.repository;
 
 import com.fullstack.bankbackend.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
