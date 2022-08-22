5c8f80e16b9c6941ccab9322a3b0a56bce8e7eea
Roberto Mendez <roberto.mendez1983@gmail.com>
2022-08-22 14:30:23 +0200

    Annotations and variables created in Client Entity

--- src/main/java/com/fullstack/bankbackend/model/Client.java
+++ src/main/java/com/fullstack/bankbackend/model/Client.java
@@ -1,9 +1,24 @@
 package com.fullstack.bankbackend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String firstName;
+
     private String lastName;
+
     private String email;
 }
